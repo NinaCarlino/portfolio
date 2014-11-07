a37de06566edfa2afc3ff11dedf148cd650111ce
Nina Pitakwong <ppitakwong@gmail.com>
2014-11-06 19:10:19 -0600

    add javascript

---
+++ js/page-transitions.js
@@ -0,0 +1,12 @@
+var scrollToMockups = function(){
+  $('html, body').animate({ scrollTop: ($("#mockups").offset().top - 75)}, 1500);
+};
+
+var scrollToAbout = function(){
+  $('html, body').animate({ scrollTop: ($("#about").offset().top - 75)}, 2000);
+};
+
+var scrollToContact = function(){
+  $('html, body').animate({ scrollTop: ($("#contact").offset().top - 75)}, 3000);
+};
+
